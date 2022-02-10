34b700874505637981a0587b23f6c85e9868c2b5
Wes Garland <wes@page.ca>
2022-02-09 23:29:57 -0500

    added start command w/ working dependency resolution

--- usr/lib/shystemd/service.mk
+++ usr/lib/shystemd/service.mk
@@ -11,6 +11,25 @@
 systemDir=$(SYSTEMD_CONF_ROOT)/shystemd-db/system
 
 include $(systemDir)/$(unit).service.mk
+-include $(systemDir)/$(unit).service.deps
+
+pidfile=$(SYSTEMD_CONF_ROOT)/shystemd-db/system/$(unit).pid
+daemon=daemon -N -F $(pidfile)
+
+#-u user:group
+#-D directory
+#-r is respawn
+#-l logfile or syslog level
+#--running - check is running
+#--stop - kill it
+#--list
+#--signal - send signal
+
+ifeq ($(Service_Type),oneshot)
+  launch=$(daemon)
+else
+  launch=$(daemon) -r
+endif
 
 status:
 	@printf "%s\t%s\n" "$(unit_basename)" "$(Unit_Description)"
@@ -18,5 +37,19 @@ status:
 .PHONY: deps
 deps:
 #	@echo Building deps - $(unit) is needed by $(Install_WantedBy)
-	@$(shell $(foreach dep, $(Install_WantedBy), echo start: $(unit) >> $(systemDir)/$(dep).deps;))
+	@$(shell $(foreach dep, $(Install_WantedBy), echo start: start-$(unit) >> $(systemDir)/$(dep).deps;))
+	@$(shell $(foreach dep, $(Install_WantedBy), echo stop: stop-$(unit) >> $(systemDir)/$(dep).deps;))
 	@true
+
+# Make this target a dependency to dump info before other target
+debug:
+	@echo "unit=$(unit)"
+	@echo "systemDir=$(systemDir)"
+	@cat $(systemDir)/$(unit).service.mk
+
+start:  
+	@echo Starting unit $(unit)
+	$(launch) -n $(unit) $(Service_ExecStart)
+
+start-%:
+	$(MAKE) unit="$*" -f "${SHYSTEMD_LIB_DIR}"/service.mk start
